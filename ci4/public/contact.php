cfe145948b82d50dc3dd66781ada20347d673022
jmazingw <jdgonzales2@student.apc.edu.ph>
2023-02-28 12:47:32 +0800

    ci4 Contact form (fix)(hopefully)

--- ci4/public/contact.php
+++ ci4/public/contact.php
@@ -1,28 +1,34 @@
 <?php
-
-$request = \Config\Services::request();
-
-if(empty($request->getPost('name')) || empty($request->getPost('subject')) || empty($request->getPost('message')) || !filter_var($request->getPost('email'), FILTER_VALIDATE_EMAIL)) {
+if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   echo json_encode(array("status" => "error"));
   exit();
 }
 
-$name = strip_tags(htmlspecialchars($request->getPost('name')));
-$email = strip_tags(htmlspecialchars($request->getPost('email')));
-$subject = strip_tags(htmlspecialchars($request->getPost('subject')));
-$message = strip_tags(htmlspecialchars($request->getPost('message')));
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email = strip_tags(htmlspecialchars($_POST['email']));
+$subject = strip_tags(htmlspecialchars($_POST['subject']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
 
-$db = \Config\Database::connect();
+$servername = "192.168.150.213";
+$username = "webprogss211";
+$password = "fancyR!ce36";
+$dbname = "webprogss211";
 
-$data = [
-    'ANSname' => $name,
-    'ANSsubject' => $subject,
-    'email' => $email,
-    'ANSmessage' => $message
-];
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 
-if ($db->table('jdgonzales2_myguests')->insert($data)) {
+$sql = "INSERT INTO jdgonzales2_myguests(ANSname, ANSsubject, email, ANSmessage)
+VALUES ('$name', '$subject', '$email', '$message')";
+
+if ($conn->query($sql) === TRUE) {
   echo json_encode(array("status" => "success"));
 } else {
   echo json_encode(array("status" => "error"));
 }
+
+$conn->close();
+?>
