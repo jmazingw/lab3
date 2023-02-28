01b4af35248f641802f10d6be64148e388928bf6
jmazingw <jdgonzales2@student.apc.edu.ph>
2023-02-28 11:59:59 +0800

    ci4 news section add

--- ci4/app/Config/Database.php
+++ ci4/app/Config/Database.php
@@ -26,7 +26,7 @@ class Database extends Config
      */
     public array $default = [
         'DSN'      => '',
-        'hostname' => 'z',
+        'hostname' => 'localhost',
         'username' => '',
         'password' => '',
         'database' => '',
--- ci4/app/Models/NewsModel.php
+++ ci4/app/Models/NewsModel.php
@@ -7,6 +7,7 @@ use CodeIgniter\Model;
 class NewsModel extends Model
 {
     protected $table = 'jdgonzales2_news';
+    protected $allowedFields = ['title', 'slug', 'body'];
     public function getNews($slug = false)
     {
         if ($slug === false) {
